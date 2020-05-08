f3fefcc3f5a12cc0d5769405437d981d51766f07
Adam Williams <adam.williams@lifeomic.com>
2020-05-07 23:40:34 -0400

    Add curl script to test auth creds

---
+++ curl-duo-api.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o allexport; source .env; set +o allexport
+
+echo "$SECRET_KEY"
+
+method="GET"
+path="/admin/v1/settings"
+params=""
+uri="https://$API_HOSTNAME$path"
+
+date2822=`date -u -R`
+authsig="$date2822
+$method
+$API_HOSTNAME
+$path
+$params"
+
+digest=`echo -n "$authsig" | openssl dgst -sha1 -hmac "$SECRET_KEY"`
+
+curl -u "$INTEGRATION_KEY:$digest" -H "Date: $date2822" $uri
